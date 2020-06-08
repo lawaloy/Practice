a243d9cda611683f5a7cbaba894900fd71583eb6
Oluwaseyi Lawal <lasei_1992@yahoo.com>
2020-06-08 00:00:17 -0500

    Add files via upload

---
+++ MagicNum.py
@@ -0,0 +1,23 @@
+def magic_num(num):
+    
+    if not num:
+        return
+    
+    ans = []
+    res = 0
+    
+    while num>0:
+        ans.append(num%2)
+        temp = num//2
+        num = temp
+        
+    for ind in range (len(ans)):
+        if ans[idx] > 0:
+            res += (ans[idx] * (7**idx))
+    return res
+
+"""
+N = len(ans)
+TIme: O(log(Num) + (N))
+Space: O(N)
+"""
