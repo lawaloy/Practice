11c646a295dbc23c026c3a8447bd62402a64e65d
Oluwaseyi Lawal <lasei_1992@yahoo.com>
2020-07-20 22:44:03 -0500

    Add files via upload

--- bubbleSort.py
+++ bubbleSort.py
@@ -6,3 +6,12 @@ def bubble_sort(data):
     return data
 data = [5,4,3,2,1,0,6,9,8]
 print(bubble_sort(data))
+
+def bubble_sort(data):
+    for num in range(1,len(data)):
+        for j in range(0,len(data)):
+            if data[j] > data[num]:
+                data[j],data[num] = data[num],data[j]
+    return data
+data = [-1,5,4,3,2,1,0,6,9,8,0]
+print(bubble_sort(data))
