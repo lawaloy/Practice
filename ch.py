89909cf6ccdfc37d3fa59bd64ab7710d99177463
Oluwaseyi Lawal <lasei_1992@yahoo.com>
2020-09-15 21:07:56 -0500

    Add files via upload

---
+++ ch.py
@@ -0,0 +1,29 @@
+def mergeSort(dataset):
+	if len(dataset) > 1:
+		mid = len(dataset)//2
+		leftSide = dataset[:mid]
+		rightSide = dataset[mid:]
+		mergeSort(leftSide)
+		mergeSort(rightSide)
+
+		i = j = k = 0
+
+		while i < len(leftSide) and j < len(rightSide):
+			if leftSide[i] < rightSide[j]:
+				dataset[k] = leftSide[i]
+				i += 1
+			else:
+				dataset[k] = rightSide[j]
+				j+=1
+			k+=1
+		while i < len(leftSide):
+			dataset[k] = leftSide[i]
+			i+=1
+			k+=1
+		while j < len(rightSide):
+			dataset[k] = rightSide[j]
+			j+=1
+			k+=1
+	return dataset
+
+print(mergeSort([2000,309]))
